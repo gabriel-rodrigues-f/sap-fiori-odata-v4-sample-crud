e6cb11cc83d1f183a03bc1b2d8dac95ecb9236cb
Gabriel Freire <gabriel.freire@lab2dev.com>
2025-09-09 19:02:51 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["com/lab2dev/ui/purchase/order/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/lab2dev/ui/purchase/order/test/unit/controller/Orders.controller"
+], function () {
+	"use strict";
+});
